73ea0a38649849e468e440a088008dcb57946c67
rizkymille <muhammad.rizky912@ui.ac.id>
2024-05-17 00:15:04 +0700

    Initial commit, WIP

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <rclcpp.hpp>
+#include <ros2_lidar_lite_v3/lidarlite_v3.h>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world ros2_lidar_lite_v3 package\n");
+  return 0;
+}
