8ffc34a999b571cd89d506fada63c19f15252893
rizkymille <muhammad.rizky912@ui.ac.id>
2024-05-17 15:38:24 +0700

    Fix cmakelist

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,32 @@
-#include <rclcpp.hpp>
-#include <ros2_lidar_lite_v3/lidarlite_v3.h>
+#include <memory>
+#include <string>
+#include <chrono>
+
+#include "rclcpp.hpp"
+#include "sensor_msgs/msg/range.hpp"
+
+#include "lidarlite_v3.h"
+
+class LidarLiteNode : public rclcpp::Node, public LIDARLite_v3 {
+  public:
+    LidarLiteNode() : Node("lidarlite_node") {
+      RCLCPP_INFO(this->get_logger(), "Initializing Garmin LiDAR-Lite v3");
+      pub_message = this->create_publisher<std_msgs::msg::String>("lidarlite_v3", 10);
+
+      timer = this->create_wall_timer(30ms, std::bind(&LidarLiteNode::reading_loop, this));
+    }
+
+  private:
+    void reading_loop {
+
+    }
+
+};
 
 int main(int argc, char ** argv)
 {
-  (void) argc;
-  (void) argv;
-
-  printf("hello world ros2_lidar_lite_v3 package\n");
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<LidarLiteNode>());
+  rclcpp::shutdown();
   return 0;
 }
